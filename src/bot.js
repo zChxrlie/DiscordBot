080a7ce389c27eed9172ad4274d621860e6c3d0d
Charlie <zcharliex@gmail.com>
2023-11-12 20:59:11 +0000

    More stuff

--- src/bot.js
+++ src/bot.js
@@ -14,19 +14,21 @@ const client = new Client({
 /*
  * Start with 'nodemon'
  * TODO:
- * Track by discord ID instead
  * Removal of strikes
 */
 
 const channelName = "strike-channel";
+const strikeRoleID = "1173045692814602271";
+const serverID = "687379271605682199"
 
 var strikes = [];
+var reasons = [];
 
 function readStrikes(cb){
     fs.readFile('src/strikes.txt', function (err, data)  {
         if (!data.toString()) return;
         data.toString().split(",").forEach((entry) => {
-            cb(entry.split(":"));
+            cb(entry.split(";"));
         });
     });
 }
@@ -37,29 +39,61 @@ readStrikes(function(data) {
     strikes.push(data);
 });
 
+function readReasons(cb){
+    fs.readFile('src/reasons.txt', function (err, data)  {
+        if (!data.toString()) return;
+        data.toString().split(",").forEach((entry) => {
+            cb(entry.split(";"));
+        });
+    });
+}
+
+console.log("Loaded reasons");
+readReasons(function(data) { 
+    // console.log(data[0] + ": " + data[1]);
+    reasons.push(data);
+});
+
 client.login(process.env.TOKEN);
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async(message) => {
     if (message.toString() == "!help") {
-        Commands.help();
+        commands.help();
         return;
     }
 
+    let guild = await client.guilds.fetch(serverID);
+    let member = await guild.members.fetch(message.author.id);
     if (message.toString().startsWith("!strike ")) {
-        let person = message.toString().substring(8).toLowerCase();
-        Commands.issueStrike(person);
+        try {
+            var regex = message.toString().match(/!strike (.*) *- *(.*)/);
+            if (member.roles.cache.has(strikeRoleID)) {
+                if (regex != null) {
+                    let person = regex[1].toLowerCase().trim();
+                    let reason = (regex[2].charAt(0).toUpperCase() + regex[2].slice(1)).trim();
+                    commands.issueStrike(person, reason);
+                    commands.issueReason(person, reason);
+                } else {
+                    chat.sendMessage("Invalid strike command syntax, please follow !strike <user> - <reason>, the dash is important", channelName);
+                }
+            } else {
+                chat.sendMessage("You don't have permission to do that :point_up: :nerd:", channelName);
+            }
+        } catch(e) {
+            console.log(e);
+        }
         return;
     }
 
     if (message.toString() == "!strikes") {
-        Commands.printStrikes();
+        commands.printStrikes();
         return;
     }
 });
 
-const Commands = {
+const commands = {
     help() {
-        const displayString = "This is a work in progress bot, its current purpose is to track user strikes.\n\nCommands:\n!strike <user>\n!strikes\n!help";
+        const displayString = "This is a work in progress bot, its current purpose is to track user strikes.\n\nCommands:\n!strike <user> - <reason>\n!strikes\n!help";
 
         const displayStrikes = new EmbedBuilder()
             .setColor(0x55FF33)
@@ -70,7 +104,7 @@ const Commands = {
         chat.sendMessage({ embeds: [displayStrikes] }, channelName);
     },
 
-    issueStrike(person) {
+    issueStrike(person, reason) {
         let foundExistingEntry = false;
 
         // Add strike if user already has one
@@ -90,31 +124,49 @@ const Commands = {
 
 
         // Save new strikes
-        let saveString = "";
-        strikes.forEach((personStrikes) => {
-            saveString += personStrikes[0] + ":" + personStrikes[1] + ",";
-        });
-        fs.writeFile('src/strikes.txt', saveString.slice(0, -1), function(err) {
-            if(err) return console.log("Saving file error",err);
-            console.log("The file was saved!");
-        });
+        utils.saveStrikes();
         
-        const strikeMessage = person.charAt(0).toUpperCase() + person.slice(1) + " has recieved a strike!";
+        const strikeMessage = person + " has received a strike for " + reason;
+        console.log(strikeMessage);
         chat.sendMessage(strikeMessage, channelName);
+    },
+
+    issueReason(person, reason) {
+        let foundExistingEntry = false;
+
+        // Add reason if user already has one
+        reasons.forEach((personReasons) => {
+            if (personReasons[0] === person) {
+                let newReasons = personReasons[1] += "|" + reason;
+                personReasons[1] = newReasons;
+                foundExistingEntry = true;
+                return;
+            }
+        });
+
+        // Add reason to new user
+        if (!foundExistingEntry) {
+            reasons.push([person, reason]);
+        }
+
+        // Save new reasons
+        utils.saveReasons();
     },
 
     printStrikes() {
         let displayString = "";
         strikes.forEach((personStrikes) => {
-            displayString += personStrikes[0].charAt(0).toUpperCase() + personStrikes[0].slice(1) + ": " + personStrikes[1] + "\n";
+            displayString += personStrikes[0] + ": " + personStrikes[1] + "\n";
+            let reasonsForPerson = reasons.find(reason => reason[0] === personStrikes[0]);
+            if (reasonsForPerson) displayString += "•" + reasonsForPerson[1].replaceAll("|", "\n•") + "\n\n";           
         });
 
         const displayStrikes = new EmbedBuilder()
-            .setColor(0x0099FF)
+            .setColor(0xFF3333)
             .setTitle('Strikes')
             .setDescription(displayString)
+            .setThumbnail('https://i.imgur.com/t7DeG7P.png')
             .setTimestamp();
-
 
         chat.sendMessage({ embeds: [displayStrikes] }, channelName);
     }
@@ -125,5 +177,35 @@ const chat = {
         client.channels.cache
             .find(channel => channel.name === channelName)
             .send(message);
+    }
+}
+
+const utils = {
+    saveStrikes() {
+        this.orderStrikes();
+
+        let saveString = "";
+        strikes.forEach((personStrikes) => {
+            saveString += personStrikes[0] + ";" + personStrikes[1] + ",";
+        });
+        fs.writeFile('src/strikes.txt', saveString.slice(0, -1), function(err) {
+            if(err) return console.log("Saving strike file error",err);
+            console.log("Strike file was saved!");
+        });
+    },
+
+    saveReasons() {
+        let saveString = "";
+        reasons.forEach((personReasons) => {
+            saveString += personReasons[0] + ";" + personReasons[1] + ",";
+        });
+        fs.writeFile('src/reasons.txt', saveString.slice(0, -1), function(err) {
+            if(err) return console.log("Saving reason file error",err);
+            console.log("Reason file was saved!");
+        });
+    },
+
+    orderStrikes() {
+        strikes.sort(function(a, b) { return b[1] - a[1]});            
     }
 }
